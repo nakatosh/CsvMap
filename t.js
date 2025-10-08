4e0620048a30f8bdb4e484c2508362aafe34ff4f
nakatosh <92030557+nakatosh@users.noreply.github.com>
2025-10-08 11:56:29 +0900

    Update t.js

--- t.js
+++ t.js
@@ -161,10 +161,10 @@ function MAK(flg) {
 
     switch (flg) {
       case 1:createMarker(KAN,LAT,LNG,'#fb1bceff',key);break;
-      case 0:createMarker(moji,LAT,LNG,'#7a797aff',key);break;
+      case 0:createMarker(moji,LAT,LNG,'#fdfdfd',key);break;
       case 3:createMarker(ho,LAT, LNG,'#047104ff',key);break;
       case 4:createMarker(ho,LAT, LNG,'#14a9ceff',key);break;
-      default:createMarker(moji,LAT, LNG,'#7a797aff',key);break;
+      default:createMarker(moji,LAT, LNG,'#fdfdfd',key);break;
     }          
 }
 
@@ -172,7 +172,7 @@ function MAK(flg) {
 function createMarker(layer, lat, lng, color, key) {
   layer.addLayer(
     L.circleMarker([lat, lng], {
-      color: '#fdfdfd', weight: 1, fillColor: color, fillOpacity: 1, radius: 10, customID: key
+      color:'#7a797aff', weight: 2, fillColor: color, fillOpacity: 1, radius: 10, customID: key
     }).on('click', function(e) { markerClick(e); })
   );
 }
@@ -202,10 +202,10 @@ function MAKall() {
 //ステータスで色を変えたい
 switch (data.mv_5) {
 	case 1: addMarkerToLayer(KAN,data,'#fb1bceff',divIcon3);        break;
-	case 0: addMarkerToLayer(moji,data,'#7a797aff',divIcon3);        break;
+	case 0: addMarkerToLayer(moji,data,'#fdfdfd',divIcon3);        break;
 	case 3: addMarkerToLayer(ho,data,'#047104ff',divIcon3);        break;
 	case 4: addMarkerToLayer(ho,data,'#14a9ceff',divIcon3);        break;
-	default: addMarkerToLayer(moji,data,'#7a797aff',divIcon3);
+	default: addMarkerToLayer(moji,data,'#fdfdfd',divIcon3);
 }   
 	cursor.continue();
 };
@@ -403,6 +403,7 @@ async function inputChange() {
     map.fitBounds(ho.getBounds());
   }
 }
+
 
 
 
